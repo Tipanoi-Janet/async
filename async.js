cfde491a3a148f10d74815c2ad5258b801a2587c
Tipanoi-Janet <tipanoijanet@gmail.com>
2023-05-11 14:24:37 +0300

    first commit

---
+++ async.js
@@ -0,0 +1,39 @@
+
+//Write an asynchronous function that accepts a message string and a #delay time in milliseconds. The function should log the message to #the console after the specified delay time.
+
+async function delaytime(message, delay) {
+    await new Promise(resolve => setTimeout(resolve, delay));
+    console.log(message);
+  }
+  
+  
+  delaytime("Hey girl!", 1000);
+  
+//You have an array of user IDs and a function getUserData(id) that returns a Promise with user data when given a user ID. Write an asynchronous function that fetches and logs the data for each user ID one by one, in sequence.
+
+async function fetchAndLogUserData(IDs) {
+    for (let id of IDs){
+
+    }
+    
+      const userData = await getUserdata(ID);
+      console.log(userData);
+    }
+  const userIds = [5, 9, 33, 67, 56,60];
+   fetchAndLogUserData(userIds);
+  
+
+
+//You have an asynchronous function performTask() that returns a Promise. The Promise resolves if the task is successful and rejects if there's an error. Write a function that calls performTask() and logs a custom success message if the task is successful, and a custom error message if there's an error.
+async function taskHandler()
+   try{
+    await performTask();
+    console.log ("Task successful!");
+
+   }
+   catch (error){
+
+   }
+   console.log("Task failed:", error);
+
+taskHandler()
